fb45ef7517159451ee831969cbf8ee7c31061bd3
Igor <igorlfs@ufmg.br>
2022-05-05 20:42:57 -0300

    refactor: remove template and implement function in src

--- lib/rank.hpp
+++ lib/rank.hpp
@@ -14,30 +14,8 @@ struct Rank {
     /// @param v, vetor a ser ordenado
     /// @param reverse, define se a ordem é crescente (false) ou decrescente
     /// (true)
-    template <typename T>
-    vector<int> static sortIndexes(const vector<T> &v, const bool &reverse) {
-        // Função implementada no header porque usar templates é chato
-        // Referẽncia: https://stackoverflow.com/a/10632266
-        vector<int> sortedIndexes(v.size());
-
-        // Cria uma sequência incremental (ie, 0, 1, 2, ...) no array
-        iota(sortedIndexes.begin(), sortedIndexes.end(), 0);
-
-        // Use lambdas para comparar os elementos do vetor original
-        if (reverse) {
-            const auto COMPARATOR = [&v](const int &a, const int &b) {
-                return v.at(a) > v.at(b);
-            };
-            stable_sort(sortedIndexes.begin(), sortedIndexes.end(), COMPARATOR);
-        } else {
-            const auto COMPARATOR = [&v](const int &a, const int &b) {
-                return v.at(a) < v.at(b);
-            };
-            stable_sort(sortedIndexes.begin(), sortedIndexes.end(), COMPARATOR);
-        }
-
-        return sortedIndexes;
-    };
+    /// @return vetor com os índices ordenados
+    vector<int> static sortIndexes(const vector<int> &v, const bool &reverse);
 
     /// @brief Construa uma lista de preferências com base nos índices dos
     /// scores ordenados
@@ -46,7 +24,7 @@ struct Rank {
     /// @param list, lista que recebe a matriz com os índices
     /// @param reverse, define se a pontuação é descrescente (true) ou crescente
     /// (false)
-    static void setPreferenceList(vector<vector<int>> &scores,
+    static void setPreferenceList(const vector<vector<int>> &scores,
                                   vector<vector<int>> &list,
                                   const bool &reverse);
 };
--- src/rank.cpp
+++ src/rank.cpp
@@ -1,6 +1,28 @@
 #include "rank.hpp"
 
-void Rank::setPreferenceList(vector<vector<int>> &scores,
+vector<int> Rank::sortIndexes(const vector<int> &v, const bool &reverse) {
+    vector<int> sortedIndexes(v.size());
+
+    // Cria uma sequência incremental (ie, 0, 1, 2, ...) no array
+    iota(sortedIndexes.begin(), sortedIndexes.end(), 0);
+
+    // Use lambdas para comparar os elementos do vetor original
+    if (reverse) {
+        const auto COMPARATOR = [&v](const int &a, const int &b) {
+            return v.at(a) > v.at(b);
+        };
+        stable_sort(sortedIndexes.begin(), sortedIndexes.end(), COMPARATOR);
+    } else {
+        const auto COMPARATOR = [&v](const int &a, const int &b) {
+            return v.at(a) < v.at(b);
+        };
+        stable_sort(sortedIndexes.begin(), sortedIndexes.end(), COMPARATOR);
+    }
+
+    return sortedIndexes;
+};
+
+void Rank::setPreferenceList(const vector<vector<int>> &scores,
                              vector<vector<int>> &list, const bool &reverse) {
     for (uint i = 0; i < scores.size(); ++i) {
         const vector<int> PREF_LIST = Rank::sortIndexes(scores.at(i), reverse);
